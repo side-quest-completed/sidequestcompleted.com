3f158d6d0649ee1707113e9e5001d499ed63b5f4
Calvin Spealman <ironfroggy@gmail.com>
2020-04-25 12:38:35 -0400

    Add episode 29

--- plugins/caster/caster.py
+++ plugins/caster/caster.py
@@ -15,9 +15,10 @@ class Podcast(Directive):
     """
 
     required_arguments = 1
-    optional_arguments = 1
+    optional_arguments = 2
     option_spec = {
         'heading': str,
+        'file': str,
     }
     final_argument_whitespace = False
     has_content = False
@@ -25,17 +26,18 @@ class Podcast(Directive):
     def run(self):
         heading = self.options.get('heading', u'')
         title = self.arguments[0].strip()
+        file_ = self.options.get('file', title)
 
         html = u"""
         <div class="podcast text-center">
             <iframe src="https://archive.org/embed/{title}" width="80%" height="30" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
 
             <div class="podcast-download">
-                <a class="btn btn-primary btn-sm" href="https://archive.org/download/{title}/{title}.mp3"><i class="fa fa-download"></i> MP3</a>
-                <a class="btn btn-primary btn-sm" href="https://archive.org/download/{title}/{title}.ogg"><i class="fa fa-download"></i> OGG</a>
+                <a class="btn btn-primary btn-sm" href="https://archive.org/download/{title}/{file}.mp3"><i class="fa fa-download"></i> MP3</a>
+                <a class="btn btn-primary btn-sm" href="https://archive.org/download/{title}/{file}.ogg"><i class="fa fa-download"></i> OGG</a>
             </div>
         </div>
-        """.format(title=title)
+        """.format(title=title, file=file_)
 
         if heading:
             html = u"""
